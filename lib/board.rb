0b061380120d69ccdcec8583518be378724826d3
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-10-01 20:29:41 -0700

    Add board and test cases

---
+++ lib/board.rb
@@ -0,0 +1,22 @@
+# frozen-string-literal: true
+
+# row n by column m Connect Four board
+class Board
+  attr_reader :board
+
+  def initialize(row = 6, column = 7)
+    @max_row = row
+    @max_column = column
+    @board = Array.new(row) { Array.new(column) }
+  end
+
+  def add_piece(piece, column)
+    return nil unless column.between?(1, @max_column)
+
+    cur_row = @max_row - 1
+    cur_row -= 1 until board[cur_row][column - 1].nil? || cur_row.negative?
+    return nil if cur_row.negative?
+
+    board[cur_row][column - 1] = piece
+  end
+end
---
+++ spec/board_spec.rb
@@ -0,0 +1,90 @@
+# frozen-string-literal: true
+
+require_relative '../lib/board'
+
+describe Board do
+  describe '#add_piece' do
+    max_column = 7
+    max_row = 6
+
+    context 'when adding any piece to an empty board' do
+      subject(:empty_board) { described_class.new }
+
+      it 'returns the piece if the piece is sucessfully added to the board' do
+        result = empty_board.add_piece(:piece, 1)
+        expect(result).to eq(:piece)
+      end
+
+      it "returns nil if the piece is added to a column bigger than #{max_column}" do
+        result = empty_board.add_piece(:piece, max_column + 1) # out of range
+        expect(result).to be_nil
+      end
+
+      it 'returns nil if the piece is added to a column less than 1' do
+        result = empty_board.add_piece(:piece, 0) # out of range
+        expect(result).to be_nil
+      end
+
+      1.upto(7) do |n|
+        it "puts the piece to the lowest row, column #{n}" do
+          expect { empty_board.add_piece(:piece, n) }.to change {
+            empty_board.board[max_row - 1][n - 1]
+          }.from(nil).to(:piece)
+        end
+      end
+    end
+
+    context 'when adding any piece with the lowest row filled' do
+      subject(:lowest_filled_board) { described_class.new }
+
+      before do # fill the lowest column
+        1.upto(7) { |n| lowest_filled_board.add_piece(:piece, n) }
+      end
+
+      1.upto(7) do |n|
+        it "puts the piece to the 2nd lowest row, column #{n}" do
+          expect { lowest_filled_board.add_piece(:piece, n) }.to change {
+            lowest_filled_board.board[max_row - 2][n - 1]
+          }.from(nil).to(:piece)
+        end
+      end
+    end
+
+    context 'when adding any piece with the lowest and 2nd lowest row filled' do
+      subject(:third_row_board) { described_class.new }
+
+      before do # fill the lowest and 2nd lowest column
+        2.times { 1.upto(7) { |n| third_row_board.add_piece(:piece, n) } }
+      end
+
+      1.upto(7) do |n|
+        it "puts the piece to the 3rd lowest row, column #{n}" do
+          expect { third_row_board.add_piece(:piece, n) }.to change {
+            third_row_board.board[max_row - 3][n - 1]
+          }.from(nil).to(:piece)
+        end
+      end
+    end
+
+    context 'when the first column is filled' do
+      subject(:filled_column_board) { described_class.new }
+
+      before do # fill the first column
+        max_column.times { filled_column_board.add_piece(:piece, 1) }
+      end
+
+      it 'does not change the board from adding new piece on a filled first column' do
+        expect { filled_column_board.add_piece(:new_piece, 1) }.not_to(change(filled_column_board, :board))
+      end
+
+      it 'returns nil from adding new piece on a filled first column' do
+        result = filled_column_board.add_piece(:new_piece, 1)
+        expect(result).to be_nil
+      end
+
+      it 'changes the board when filling 2nd column' do
+        expect { filled_column_board.add_piece(:new_piece, 2) }.to(change(filled_column_board, :board))
+      end
+    end
+  end
+end
