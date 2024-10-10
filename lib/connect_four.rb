d0529de327713944cac46a790eeb35b90f0f6a76
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-10-10 01:09:41 -0700

    Add gameplay loop and tests

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,7 +1,23 @@
 # frozen-string-literal: true
 
+require_relative('board')
+
 # A game of connect four
 class ConnectFour
+  def initialize(board = Board.new)
+    @board = board
+    @players = %i[A B]
+  end
+
+  def play_game
+    turn = 0
+    loop do
+      player = @players[turn % 2]
+      turn += 1 unless player_turn(player).nil?
+      break if game_over?(player)
+    end
+  end
+
   def transform_input(input)
     return nil unless input.match?(/^\s*[1-9]+\s*$/)
 
@@ -16,5 +32,23 @@ class ConnectFour
       validated_input = transform_input(input)
       validated_input.nil? ? puts("#{input} is not a valid input") : (return validated_input)
     end
+  end
+
+  private
+
+  def player_turn(player)
+    puts "Player's #{player} turn"
+    @board.add_piece(player, player_input)
+  end
+
+  def game_over?(player)
+    if @board.check_winner
+      puts "Congratulations! #{player}"
+      return true
+    elsif @board.tie?
+      puts "It's a tie!"
+      return true
+    end
+    false
   end
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,8 +1,65 @@
 # frozen-string-literal: true
 
 require_relative '../lib/connect_four'
+require_relative '../lib/board'
 
 describe ConnectFour do
+  describe '#play_game' do
+    subject(:game_ends) { described_class.new(end_board) }
+
+    let(:end_board) { instance_double(Board) }
+
+    before do
+      allow(end_board).to receive(:add_piece)
+      allow(game_ends).to receive(:player_input)
+    end
+
+    context 'when the board detects an ending sequence' do
+      it 'sends the message to check if there is a winner once if there is a winning sequence' do
+        allow(end_board).to receive_messages(check_winner: true)
+        game_ends.play_game
+        expect(end_board).to have_received(:check_winner).once
+      end
+
+      it 'ends the game if a tie is detected' do
+        allow(end_board).to receive_messages(check_winner: false, tie?: true)
+        game_ends.play_game
+        expect(end_board).to have_received(:tie?).once
+      end
+
+      it 'ends the game if there is a winner after 4 moves' do
+        allow(end_board).to receive(:tie?).and_return(false)
+        allow(end_board).to receive(:check_winner).and_return(false, false, false, true)
+        game_ends.play_game
+        expect(end_board).to have_received(:check_winner).exactly(4).times
+      end
+
+      it 'ends the game if there is a tie after 4 moves' do
+        allow(end_board).to receive(:tie?).and_return(false, false, false, true)
+        allow(end_board).to receive(:check_winner).and_return(false)
+        game_ends.play_game
+        expect(end_board).to have_received(:tie?).exactly(4).times
+      end
+    end
+
+    context 'when the game is in progress' do
+      before do
+        allow(end_board).to receive(:tie?).and_return(false)
+        allow(end_board).to receive(:check_winner).and_return(false, false, false, true)
+      end
+
+      it "asks for player's input at least once" do
+        game_ends.play_game
+        expect(game_ends).to have_received(:player_input).at_least(1)
+      end
+
+      it 'sends message to add piece at least once' do
+        game_ends.play_game
+        expect(end_board).to have_received(:add_piece).at_least(1)
+      end
+    end
+  end
+
   describe '#player_input' do
     subject(:game_input) { described_class.new }
 
